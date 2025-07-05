3a147f2e21efe91a9d0cef7756ef54cec965b619
Rishav05112003 <rishavdeep.skills@gmail.com>
2025-07-05 21:02:12 +0530

    FIX:User auth issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+export default clerkMiddleware((auth, req) => {
+  // Allow Clerk Webhook to bypass auth
+  if (req.nextUrl.pathname === '/api/clerk-webhook') {
+    return NextResponse.next();
+  }
+
+  return NextResponse.next(); // Other logic as needed
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next|.*\\..*).*)',
     '/(api|trpc)(.*)',
   ],
-}
+};
